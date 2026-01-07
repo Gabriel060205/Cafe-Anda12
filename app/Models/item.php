da7401b114c8483e2786491cf777567ae0c9614e
Gabriel060205 <gabrielsalwey0@gmail.com>
2026-01-08 04:54:57 +0700

    Add models for Category, Item, Order, OrderItem, Role, and update User model with soft deletes

---
+++ app/Models/item.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class item extends Model
+{
+    use softDeletes, HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'img',
+        'is_active',
+        'created_at',
+        'updated_at',
+    ];
+    protected $dates = ['deleted_at'];
+
+    public function category()
+    {
+        return $this->belongsTo(Categoty::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
