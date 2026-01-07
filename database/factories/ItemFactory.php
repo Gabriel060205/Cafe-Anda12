dd371b9f7a4be38ec3db1447875b40fd141f057c
Gabriel060205 <gabrielsalwey0@gmail.com>
2026-01-08 04:51:44 +0700

    Add factories and seeders for Item, Category, Role, and User models

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'category_id' => $this->faker->numberBetween(1, 2),
+            'price' => $this->faker->randomFloat(2, 1000, 10000),
+            'description' => $this->faker->text(),
+            'img' => $this->faker->imageUrl(),
+            'is_active' => $this->faker->boolean(),
+            
+           
+            
+            'image' => null,
+            'is_available' => $this->faker->boolean(80),
+        ];
+    }
+}
